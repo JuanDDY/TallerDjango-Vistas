0955424452221b65445f2a5dad100f7236e0fa09
JuanDDY <j.duartey@uniandes.edu.co>
2024-02-10 18:36:13 -0500

    cambios

--- monitoring/settings.py
+++ monitoring/settings.py
@@ -78,11 +78,11 @@ WSGI_APPLICATION = 'monitoring.wsgi.application'
 DATABASES = {
      "default": {
         "ENGINE": "django.db.backends.postgresql_psycopg2",
-        "NAME": "<nombrebd>",
-        "USER": "<usuario>",
-        "PASSWORD": "<clave>",
+        "NAME": "base01",
+        "USER": "juanduarte",
+        "PASSWORD": "juanduarte",
         "HOST": "localhost",
-        "PORT": "",
+        "PORT": "5432",
     }
 }
 
